772549c63b82f90429665d3b174c1e0a1d8594c5
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2025-02-28 03:32:36 -0500

    Assignment 2 done

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ console.log(a);
 //Gives us the type of variable
 let num = 10;
 console.log(typeof(num),num);
+
+//Assignment 2
+let firstNo = 10;
+let secondNo = 44;
+let addition = firstNo + secondNo;
+let sub = firstNo - secondNo;
+let mult = firstNo * secondNo;
+let div = firstNo/secondNo;
+console.log(firstNo%secondNo);
+console.log(firstNo**secondNo);
